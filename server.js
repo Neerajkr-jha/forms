3eae75cb1c48557ac497852b7c8378096d7aadfd
Neerajkr-jha <neeraj.23b1541133@abes.ac.in>
2025-09-05 18:04:46 +0530

    cors updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express(); // <-- this creates app
 const PORT = process.env.PORT ||5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 mongoose
